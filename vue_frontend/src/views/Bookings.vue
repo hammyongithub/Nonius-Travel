0702db449cb9a37d428bc048597e4d3dded459af
Jorge Domingues <jorge@domingues.tech>
2023-11-22 18:15:45 +0000

    iata selector

--- vue_frontend/src/views/Bookings.vue
+++ vue_frontend/src/views/Bookings.vue
@@ -1,34 +1,37 @@
-<!-- <v-container>
-  <v-row>
-    <v-col cols="12" v-for="(hotel, index) in hotels.slice(0, 10)" :key="index">
-      <hotel-card :hotel="hotel"></hotel-card>
-    </v-col>
-  </v-row>
-</v-container> -->
-
 <template>
   <div>
   <v-form>
     <v-container>
       <v-row>
         <v-col cols="12" sm="6" md="4">
-          <v-text-field v-model="location" label="Location" outlined dense></v-text-field>
-          <v-text-field v-model="checkInDate" label="Check-in Date" type="date" outlined dense></v-text-field>
-          <v-text-field v-model="checkOutDate" label="Check-out Date" type="date" outlined dense></v-text-field>
-          <v-text-field v-model="adults" label="Guests" type="number" outlined dense></v-text-field>
-          <v-text-field v-model="roomQuantity" label="Rooms" type="number" outlined dense></v-text-field>
-          <v-text-field v-model="priceMin" label="Minimum Price" type="number" outlined dense></v-text-field>
-          <v-text-field v-model="priceMax" label="Maximum Price" type="number" outlined dense></v-text-field>
+          <v-combobox
+            v-model="location"
+            :items="iataCodes"
+            :filter="filterSearch"
+            label="Location"
+            item-text="label"
+            item-value="value"
+            clearable
+            outlined
+            dense
+          ></v-combobox>
+          <v-text-field type="date" v-model="checkInDate" label="Check-in Date" outlined dense></v-text-field>
+          <v-text-field type="date" v-model="checkOutDate" label="Check-out Date" outlined dense></v-text-field>
+          <v-text-field type="number" v-model="adults" label="Guests" outlined dense></v-text-field>
+          <v-text-field type="number" v-model="roomQuantity" label="Rooms" outlined dense></v-text-field>
+          <v-text-field type="number" v-model="priceMin" label="Minimum Price" outlined dense></v-text-field>
+          <v-text-field type="number" v-model="priceMax" label="Maximum Price" outlined dense></v-text-field>
           <v-text-field v-model="currency" label="Currency" outlined dense></v-text-field>
           <v-text-field v-model="boardType" label="Board Type" outlined dense></v-text-field>
         </v-col>
       </v-row>
       <v-btn @click="searchHotels" color="primary" dark>Search Hotels</v-btn>
       <v-btn @click="searchOffers" color="primary" dark>Search Offers</v-btn>
+      <v-btn @click="print" color="primary" dark>prints</v-btn>
     </v-container>
     <v-container>
       <v-row>
-        <v-col cols="12" v-for="(offer, index) in hotelOffers" :key="index">
+        <v-col cols="12" v-for="(offer, index) in hotelOffers.slice(0, 9)" :key="index">
           <hotel-card :offer="offer"></hotel-card>
         </v-col>
       </v-row>
@@ -41,6 +44,7 @@
 <script>
 import axios from 'axios'
 import HotelCard from '../components/HotelCard.vue'
+import airports from '../assets/airports.json'
 
 export default {
   components: {
@@ -48,6 +52,7 @@ export default {
   },
   data() {
     return {
+      iataJSON: airports,
       location: '',
       hotels: [],
       hotelIds: [],
@@ -62,12 +67,19 @@ export default {
       hotelOffers: [],
     }
   },
+  computed: {
+    iataCodes() {
+      return this.iataJSON.map((airport) => `${airport.iata} - ${airport.country} - ${airport.city}`);
+    },
+  },
+
   methods: {
     searchHotels() {
+      const locationCode = this.location.substring(0, 3);
       axios
         .get('http://127.0.0.1:8000/api/v1/hotelsearch/', {
           params: {
-            location: this.location,
+            location: locationCode,
           },
         })
         .then((response) => {
@@ -85,18 +97,17 @@ export default {
     },
     searchOffers() {
     const hotelIdsString = JSON.stringify(this.hotelIds);
-
     axios
         .get('http://127.0.0.1:8000/api/v1/offersearch/', {
             params: {
                 hotelIds: hotelIdsString,
-                // adults: this.adults,
-                // checkInDate: this.checkInDate,
-                // checkOutDate: this.checkOutDate,
-                // roomQuantity: this.roomQuantity,
-                // priceRange: this.priceMin.toString() + '-' + this.priceMax.toString(),
-                // currency: this.currency,
-                // boardType: this.boardType,
+                adults: this.adults,
+                checkInDate: this.checkInDate,
+                checkOutDate: this.checkOutDate,
+                roomQuantity: this.roomQuantity,
+                priceRange: this.priceMin.toString() + '-' + this.priceMax.toString(),
+                currency: this.currency,
+                boardType: this.boardType,
             },
         })
         .then((response) => {  
@@ -108,6 +119,15 @@ export default {
             console.error('Error fetching hotel offers:', error);
         });
     },
+    filterSearch(item, queryText, itemText) {
+      const text = item.label.toLowerCase();
+      const searchText = queryText.toLowerCase();
+
+      return text.indexOf(searchText) > -1;
+    },
+    print() {
+      console.log(this.location)
+    }
   }
 }
 </script>
--- vue_frontend/src/views/HomeView.vue
+++ vue_frontend/src/views/HomeView.vue
@@ -9,10 +9,11 @@
           </v-row>
           <v-row>
             <v-col cols="6">
-              <v-card image="src/assets/wave.jpg" to="/dashboard">
+              <v-card to="/dashboard">
                 <v-card-title primary-title>
                     <h3 class="text-h6 mb-1">DashBoard</h3>
                   </v-card-title>
+                    <v-divider></v-divider>
                     <v-list lines="3">
                       <v-list-item class="text-caption">Overview of sales figures</v-list-item>
                       <v-list-item class="text-caption">Top-selling destinations</v-list-item>
@@ -21,10 +22,11 @@
               </v-card>
             </v-col>
             <v-col cols="6">
-              <v-card image="src/assets/wave.jpg" to="/customers">
+              <v-card to="/customers">
                 <v-card-title primary-title>
                     <h3 class="text-h6 mb-1">Customer Management</h3>
                   </v-card-title>
+                    <v-divider></v-divider>
                     <v-list lines="3">
                       <v-list-item class="text-caption">Customer profiles with contact information and booking history</v-list-item>
                       <v-list-item class="text-caption">CRM tools for marketing, reminders, and feedbacks</v-list-item>
@@ -35,10 +37,11 @@
           </v-row>
           <v-row>
             <v-col cols="6">
-              <v-card image="src/assets/wave.jpg" to="/bookings">
+              <v-card to="/bookings">
                 <v-card-title primary-title>
                     <h3 class="text-h6 mb-1">Booking Management</h3>
                   </v-card-title>
+                    <v-divider></v-divider>
                     <v-list lines="3">
                       <v-list-item class="text-caption">Create, update, and cancel bookings</v-list-item>
                       <v-list-item class="text-caption">Manage individual and group bookings</v-list-item>
@@ -47,10 +50,11 @@
               </v-card>
             </v-col>
             <v-col cols="6">
-              <v-card image="src/assets/wave.jpg" to="/packages">
+              <v-card to="/packages">
                 <v-card-title primary-title>
                     <h3 class="text-h6 mb-1">Package and Itinerary Management</h3>
                   </v-card-title>
+                  <v-divider></v-divider>
                     <v-list lines="3">
                       <v-list-item class="text-caption">Add, edit, or delete tour packages</v-list-item>
                       <v-list-item class="text-caption">Define itineraries with details on destinations, accommodations, and activities</v-list-item>
